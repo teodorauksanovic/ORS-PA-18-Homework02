9e7b7564f036d9a3caef2ccc181d55d890e8926f
teodorauksanovic <teodora.uksanovic@udg.edu.me>
2019-02-28 23:43:11 +0100

    zad2

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 def can_string_be_float(user_value):
 
-    dozvoljeni_karakteri = ['0','1','2','3','4','5','6','7','8','9','.','-']
+    dozvoljeni_karakteri = ['0','1','2','3','4','5','6','7','8','9','.']
 
     for ch in user_value:
 
