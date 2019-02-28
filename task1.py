73dd464e9a6d64f49d71ce69e61650f41f443c04
teodorauksanovic <teodora.uksanovic@udg.edu.me>
2019-02-28 13:44:23 +0100

    zad1

--- task1.py
+++ task1.py
@@ -15,11 +15,46 @@
 ===================================================
 """
 
+def can_string_be_float(user_value):
+
+    dozvoljeni_karakteri = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-']
+
+    for ch in user_value:
+        if ch not in dozvoljeni_karakteri:
+            return False
+
+        broj_tacaka = 0
+
+        for ch in user_value:
+            if ch == '.':
+                broj_tacaka = broj_tacaka + 1
+
+        if broj_tacaka > 1:
+            return False
+
+        broj_minusa = 0
+
+        for ch in user_value:
+            if ch == '-':
+                broj_minusa = broj_minusa + 1
+
+        if broj_minusa > 1:
+            return False
+
+        if broj_minusa == 1:
+            if user_value[0] != '-':
+                return False
+
+
+
+    return True
+
 
 def main():
 
     user_value = input("Enter string which will be evaluated: ")
-    print(can_string_be_float(user_value))
-    print(float(user_value))
-
+    if can_string_be_float(user_value):
+        print(float(user_value))
+    else:
+        print("Unijeti string se ne moze pretvoriti u float.")
 main()
