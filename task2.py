45acd5d27756ab7a00dbf82ca4ec39ef1d2d629b
teodorauksanovic <teodora.uksanovic@udg.edu.me>
2019-02-28 23:40:32 +0100

    zad2

--- task2.py
+++ task2.py
@@ -1,50 +1,32 @@
-"""
-===================   TASK 2   ====================
-* Name: Convert Kilometers To Miles
-*
-* Write a script that will convert kilometers to
-* miles. Script should ask user for input and check
-* if user input is valid. If not, the script should
-* quit immediately with appropriate message.
-*
-*
-* Note: Please describe in details possible cases
-* in which your solution might not work.
-*
-* Note: You may use `can_string_be_float` function
-* from previous task to validate user input.
-===================================================
-"""
-
-
 def can_string_be_float(user_value):
-    allowed_characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+
+    dozvoljeni_karakteri = ['0','1','2','3','4','5','6','7','8','9','.','-']
 
     for ch in user_value:
-        if ch not in allowed_characters:
+
+        if ch not in dozvoljeni_karakteri:
             return False
 
-        number_of_dots = 0
+        broj_tacaka = 0
 
         for ch in user_value:
             if ch == '.':
-                number_of_dots = number_of_dots + 1
+                broj_tacaka = broj_tacaka + 1
 
-        if number_of_dots > 1:
+        if broj_tacaka > 1:
             return False
+
+
 
     return True
 
-
-
 def main():
-    user_value = float(input("Enter a distance in miles: "))
 
-    conversionfactor = 1.609
-    kilometers = user_value * conversionfactor
+    user_value= (input("Unesite duzinu u kilometrima: "))
 
     if can_string_be_float(user_value):
-        print("The distance in kilometers is: ", kilometers)
+        print("Duzina u miljama je: ", float(user_value) * 0.6214)
     else:
-        print("User value is not valid.")
+        print("Vas unos nije validan.")
+
 main()
